3206f21d7ea16602b44e72f1617ef6f15fb64c5a
ragoupady <rsagadevin@clever-age.com>
2016-02-20 15:19:29 +0100

    Comment sur une partie de config.php

--- web/config.php
+++ web/config.php
@@ -1,8 +1,8 @@
 <?php
 
-if (!isset($_SERVER['HTTP_HOST'])) {
-    exit('This script cannot be run from the CLI. Run it from a browser.');
-}
+//if (!isset($_SERVER['HTTP_HOST'])) {
+//    exit('This script cannot be run from the CLI. Run it from a browser.');
+//}
 
 //if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
 //    '127.0.0.1',
